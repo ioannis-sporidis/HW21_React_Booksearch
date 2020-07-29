4596257969bc471c1334cc7488d42ba568992bcf
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-07-29 14:44:09 -0400

    Pushing the build forlder

---
+++ client/build/precache-manifest.f15618783b1bec859c854726e2ccdc58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c818e737324cfe32b61d3fbfe939ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97a48a4fa56957a7eb0f",
+    "url": "/static/js/2.ff80bc4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ff80bc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6503a5857a365aa9ad3",
+    "url": "/static/js/main.d35fe925.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
